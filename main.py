eb16f1f90add3d791c5179cd44e07ab8e50ca0c0
SeeMemes <provotorov-02@mail.ru>
2021-05-06 13:44:07 +0300

    v1.1

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import math
-import numpy as np
 import matplotlib.pyplot as plt
 from termcolor import cprint
 import scipy.stats as st
-from tabulate import tabulate
 
 
 ##############################
@@ -41,22 +39,59 @@ def empirical_mass(prev):
             new_mass.append(k / len(prev))
             sum += k / len(prev)
             k = 1
-            print('\tif ' + str(prev[i]) + ' < x < ' + str(prev[i + 1]) + ' then f(x) = ' + str(round(sum*100)/100))
+            print('\tif ' + str(prev[i]) + ' < x < ' + str(prev[i + 1]) + \
+                  ' then f(x) = ' + str(round(sum * 100) / 100) + \
+                  ' and the middle is ' + str(round(prev[i] - prev[i + 1])))
     new_mass.append(k / len(prev))
     sum += k / len(prev)
-    print('\tif x > ' + str(prev[len(prev)-1]) + ' then ' + ' f(x) = ' + str(round(sum*100)/100))
+    print('\tif x > ' + str(prev[len(prev) - 1]) + ' then ' + ' f(x) = ' + str(round(sum * 100) / 100))
 
     return new_mass
+
 
 def remove_dupes(prev):
     prev = list(dict.fromkeys(prev))
     return prev
+
 
 def plot_empirical_cdf(sample):
     plt.hist(sample, histtype='step', cumulative=True, bins=len(sample) * 10)
     plt.title('График эмперической функции распределения')
     plt.show()
 
+
+def line(y1, y2, x1, x2, x):
+    if (x2 - x1 != 0):
+        k = (y2 - y1) / (x2 - x1)
+        return k * (x - x1) / y1
+    else:
+        return x1
+
+
+def mass_get(num, h):
+    mass = [[], []]
+    j = h / 2
+    k = 0
+    for i in range(1, len(num)):
+        if (i==len(num)-1):
+            k += 2
+            j -= num[i] - num[i - 1]
+            mass[0].append(mass[0][len(mass[0])-1]+h)
+            mass[1].append(k)
+        elif ((j - (num[i] - num[i - 1])) >= 0):
+            k += 1
+            j -= (num[i] - num[i - 1])
+        else:
+            k += 1
+            j -= num[i] - num[i - 1]
+            if len(mass[0]) == 0:
+                mass[0].append(num[i] + j - h/2)
+            else:
+                mass[0].append(mass[0][len(mass[0]) - 1] + h)
+            mass[1].append(k)
+            k = 0
+            j = h
+    return mass
 
 ##############################
 #     PROGRAM BEGINNING      #
@@ -77,10 +112,23 @@ cprint('Эмперическая функция распределения: ', '
 e_mass = empirical_mass(mass)
 plot_empirical_cdf(mass)
 nodupe = remove_dupes(mass)
-plt.plot(e_mass)
+
+cprint('Величина интервала: ', 'green')
+h = (nodupe[len(nodupe) - 1] - nodupe[0]) / (1 + math.log(len(mass), 2))
+
+massive = mass_get(nodupe, h)
+
+mass_x = massive[0]
+mass_n = massive[1]
+mass_p = []
+for i in range (len(mass_n)):
+    mass_p.append(mass_n[i]/len(nodupe))
+
+print('\t' + str(h))
+plt.plot(mass_x, mass_p)
 plt.title('Полигон частот групированной выборки')
 plt.show()
-plt.bar(nodupe, width=0.2, height=e_mass)
+plt.bar(mass_x, width=h, height=mass_p)
 st.gaussian_kde(nodupe)
 plt.title('Гистограмма частот групированной выборки')
 plt.show()
